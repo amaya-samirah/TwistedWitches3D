bda892e02b23c237609ca28962db4b5e10f4e199
Amaya Shabazz <amaya.s.26292@gmail.com>
2025-10-28 12:30:41 -0400

    Added functionality to Start Menu UI

--- Twisted Witches 3D/Assets/Scripts/StartMenu.cs
+++ Twisted Witches 3D/Assets/Scripts/StartMenu.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+// SCENE LIST:
+// 0 - Start Menu
+// 1 - Home Lot
+// 2 - Home (interior)
+// 3 - Town
+// 4 - Dark Forest
+
+// The main functionality for the Start Menu UI
 public class StartMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Called by Play button
+    public void PlayButton()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Called by New Game button
+    public void NewGameButton()
     {
-        
+        // TODO:
+        // - create new game (save controller)
+        SceneManager.LoadScene(1);
+    }
+
+    // Called by Quit button
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
