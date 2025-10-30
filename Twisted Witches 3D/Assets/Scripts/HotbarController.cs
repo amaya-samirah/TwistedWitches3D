40f172c1d857035892a9aab45366497fea82254d
Amaya Shabazz <amaya.s.26292@gmail.com>
2025-10-30 11:54:18 -0400

    Added hotbar

--- Twisted Witches 3D/Assets/Scripts/HotbarController.cs
+++ Twisted Witches 3D/Assets/Scripts/HotbarController.cs
@@ -1,16 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HotbarController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject hotbarPanel;
+    public GameObject slotPrefab;
+    public GameObject itemDictionaryObject;
+    public int slotCount = 9;  // 1-9 on keyboard
+    public Item usingItem { get; private set; }
+
+    private ItemDictionary itemDictionary;
+
+    private Key[] hotbarKeys;
+
+    void Awake()
     {
-        
+        itemDictionary = itemDictionaryObject.GetComponent<ItemDictionary>();
+
+        hotbarKeys = new Key[slotCount];
+        for (int i = 0; i < slotCount; i++)
+        {
+            // If slot less than 9 store keye of number of slot + 1
+            hotbarKeys[i] = i < 9 ? (Key)((int)Key.Digit1 + i) : Key.Digit0;  // slot 0 is first slot on hotbar so need to set to digit 1
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check for key presses
+        for (int i = 0; i < slotCount; i++)
+        {
+            if (Keyboard.current[hotbarKeys[i]].wasPressedThisFrame)
+            {
+                // Use item
+                UseItemInSlot(i);
+                Debug.Log("Keyboard pressed.");
+            }
+        }
+    }
+
+    void UseItemInSlot(int index)
+    {
+        Slot slot = hotbarPanel.transform.GetChild(index).GetComponent<Slot>();  // get child matching index being pressed
+
+        if (slot.currentItem != null)
+        {
+            Item item = slot.currentItem.GetComponent<Item>();
+            if (item.hasDuration)
+            {
+                StartCoroutine(DurationRemove(slot, item));
+                return;
+            }
+            item.UseItem();
+            usingItem = item;
+        }
+        else
+        {
+            usingItem = null;
+            Debug.Log("slot.currentItem is null");
+        }
+    }
+
+    IEnumerator DurationRemove(Slot slot, Item item)
+    {
+        item.UseItem();
+        Debug.Log("Waiting for duration.");
+        if (item.quantity == 1)
+        {
+            RemoveItem(slot, item);
+        }
+        else
+        {
+            item.RemoveFromStack();
+        }
+        yield return new WaitForSeconds(item.duration);
+    }
+
+    private void RemoveItem(Slot slot, Item item)
+    {
+        Debug.Log($"Removing item: {slot.currentItem.name}.");
+        if (slot.currentItem != null && item != null)
+        {
+            if (item.quantity == 1)
+            {
+                Destroy(slot.currentItem);
+            }
+            else
+            {
+                item.RemoveFromStack();
+            }
+        }
+    }
+
+    // Load saved hotbar
+    public List<InventorySaveData> GetHotBarItems()
+    {
+        List<InventorySaveData> hotbarData = new List<InventorySaveData>();
+        foreach (Transform slotTransform in hotbarPanel.transform)  // for every slot in inventory panel
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot.currentItem != null)
+            {
+                Item item = slot.currentItem.GetComponent<Item>();
+                // GetSiblingIndexgets index of game object in conjunction to other game objects on same level (i.e. where they sit on child hierarchy)
+                hotbarData.Add(new InventorySaveData { itemID = item.ID, slotIndex = slotTransform.GetSiblingIndex() });
+            }
+        }
+
+        return hotbarData;
+    }
+    
+    // Save hotbar
+    public void SetHotbarItems(List<InventorySaveData> inventorySaveData)
+    {
+        // Clear out inventory panel
+        foreach (Transform child in hotbarPanel.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Create new slots
+        for (int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab, hotbarPanel.transform);  // sets hotbar panel to parent of slot prefab
+        }
+
+        // Populate slots with saved items
+        foreach (InventorySaveData data in inventorySaveData)
+        {
+            if (data.slotIndex < slotCount)  // can actually fit in hotbar
+            {
+                Slot slot = hotbarPanel.transform.GetChild(data.slotIndex).GetComponent<Slot>();
+                GameObject itemPrefab = itemDictionary.GetItemPrefab(data.itemID);
+
+                if (itemPrefab != null)
+                {
+                    Vector3 smallerScale = new Vector3(0.5f, 0.5f, 1);
+                    GameObject item = Instantiate(itemPrefab, slot.transform);  // creating new object of item prefab from dictionary and putting in in slot
+                    item.transform.localScale = smallerScale;
+                    item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                    slot.currentItem = item;
+                }
+            }
+        }
     }
 }
--- Twisted Witches 3D/Assets/Scripts/InventoryController.cs
+++ Twisted Witches 3D/Assets/Scripts/InventoryController.cs
@@ -10,6 +10,7 @@ public class InventoryController : MonoBehaviour
     public static InventoryController Instance { get; private set; }
 
     public GameObject itemDictionaryObject;
+    public GameObject hotbarControllerObject;
     public GameObject inventoryPanel;
     public GameObject slotPrefab;
     public int slotCount;
@@ -35,18 +36,33 @@ public class InventoryController : MonoBehaviour
     {
         if (itemDictionaryObject != null) itemDictionary = itemDictionaryObject.GetComponent<ItemDictionary>();
         // add hotbar script to gamecontroller
-        hotbarController = GetComponentInParent<HotbarController>();
+        hotbarController = hotbarControllerObject.GetComponent<HotbarController>();
 
         // Build item counts
+        RebuildItemCounts();
     }
 
     public void RebuildItemCounts()
     {
         itemsCountCache.Clear();
 
-        // TODO:
-        // - check hotbar
+        // Check hotbar
+        foreach (Transform slotTransform in hotbarController.hotbarPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot.currentItem != null)
+            {
+                Item item = slot.currentItem.GetComponent<Item>();
 
+                if (item != null)
+                {
+                    // Adding this item's quantity to any existing one inside cache
+                    itemsCountCache[item.ID] = itemsCountCache.GetValueOrDefault(item.ID, 0) + item.quantity;
+                }
+            }
+        }
+
+        // Check inventory
         foreach (Transform slotTransform in inventoryPanel.transform)
         {
             Slot slot = slotTransform.GetComponent<Slot>();
@@ -73,8 +89,25 @@ public class InventoryController : MonoBehaviour
         Item itemToAdd = itemPrefab.GetComponent<Item>();
         if (itemToAdd == null) return false;
 
-        // TODO:
-        // - check hotbar 1st
+        // Check hotbar
+        // Look for slot with same item
+        foreach (Transform slotTransform in hotbarController.hotbarPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot != null && slot.currentItem != null)
+            {
+                Item slotItem = slot.currentItem.GetComponent<Item>();
+                if (slotItem != null && slotItem.ID == itemToAdd.ID)
+                {
+                    // Stack
+                    slotItem.AddToStack();
+
+                    RebuildItemCounts();
+
+                    return true;
+                }
+            }
+        }
 
         // Check inventory
         foreach (Transform slotTransform in inventoryPanel.transform)  // for every slot inside inventory panel
@@ -96,8 +129,23 @@ public class InventoryController : MonoBehaviour
             }
         }
 
-        // TODO:
-        // - add to hotbar 1st
+        // Add items to hotbar 1st if available slot
+        foreach (Transform slotTransform in hotbarController.hotbarPanel.transform)  // for every slot inside inventory panel
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot != null && slot.currentItem == null)
+            {
+                GameObject newItem = Instantiate(itemPrefab, slotTransform);
+                newItem.transform.localScale = new Vector3(0.5f, 0.5f, 1);  // make item look smaller in slot
+                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+
+                slot.currentItem = newItem;
+                Debug.Log("Added to hotbar.");
+
+                RebuildItemCounts();
+                return true;
+            }
+        }
 
         // Look for empty slot in inventory
         foreach (Transform slotTransform in inventoryPanel.transform)  // for every slot inside inventory panel
@@ -115,6 +163,7 @@ public class InventoryController : MonoBehaviour
             }
         }
 
+        Debug.Log("Inventory is full.");
         return false;  // not added
     }
 
@@ -187,8 +236,26 @@ public class InventoryController : MonoBehaviour
     // To remove item from inventory
     public void RemoveItems(int itemID, int amount)
     {
-        // TODO:
-        // -check hotbar
+        // Check hotbar
+        foreach (Transform slotTransform in hotbarController.hotbarPanel.transform)
+        {
+            if (amount <= 0) break;
+
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot?.currentItem?.GetComponent<Item>() is Item item && item.ID == itemID)  // if item isn't null grab it and check if ID matches
+            {
+                int removed = Mathf.Min(amount, item.quantity);
+                item.RemoveFromStack(removed);
+                amount -= removed;
+
+                // Destory if all items removed
+                if (item.quantity == 0)
+                {
+                    Destroy(slot.currentItem);
+                    slot.currentItem = null;
+                }
+            }
+        }
         
         // Check inventory
         foreach (Transform slotTransform in inventoryPanel.transform)
